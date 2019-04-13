cdfc0f65f2a8f6302dbb4199efd73d29c58cf2d3
Matheus Brandino <m.v.brandino@gmail.com>
2019-04-13 09:18:33 -0300

    exibindo tela de login

---
+++ app/src/main/java/br/com/caelum/casadocodigoapp/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package br.com.caelum.casadocodigoapp.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.caelum.casadocodigoapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
