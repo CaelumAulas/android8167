9bbd4660978b542379ed93a9dbd32346b6cdd684
Matheus Brandino <m.v.brandino@gmail.com>
2019-04-13 10:13:50 -0300

    criando user no firebase

--- app/src/main/java/br/com/caelum/casadocodigoapp/activity/LoginActivity.kt
+++ app/src/main/java/br/com/caelum/casadocodigoapp/activity/LoginActivity.kt
@@ -1,14 +1,35 @@
 package br.com.caelum.casadocodigoapp.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.caelum.casadocodigoapp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-    }
 
+        val auth = FirebaseAuth.getInstance()
+
+        login_novo.setOnClickListener {
+
+            val email = login_email.text.toString()
+            val senha = login_senha.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener { task ->
+
+                    if (task.isSuccessful) {
+                        val vaiParaLista = Intent(this, LivroActivity::class.java)
+                        startActivity(vaiParaLista)
+                        finish()
+                    }
+                }
+        }
+
+    }
 }
--- app/src/main/java/br/com/caelum/casadocodigoapp/application/CasaDoCodigoApplication.kt
+++ app/src/main/java/br/com/caelum/casadocodigoapp/application/CasaDoCodigoApplication.kt
@@ -2,6 +2,7 @@ package br.com.caelum.casadocodigoapp.application
 
 import android.app.Application
 import br.com.caelum.casadocodigoapp.di.modulos
+import com.google.firebase.FirebaseApp
 import org.koin.android.ext.android.startKoin
 
 class CasaDoCodigoApplication : Application() {
@@ -9,5 +10,7 @@ class CasaDoCodigoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, modulos)
+
+        FirebaseApp.initializeApp(this)
     }
 }
